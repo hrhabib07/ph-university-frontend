590d72f049c7a5fd0a0dc1572780cf2bdb15e2e2
Hr Habib <mdhabibur.hr7@gmail.com>
2024-07-04 08:42:48 +0600

    first commit

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,74 @@
+import { Layout, Menu, MenuProps } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Profile",
+  },
+  {
+    key: "2",
+    label: "User Management",
+    children: [
+      {
+        key: "31",
+        label: "Create Admin",
+      },
+      {
+        key: "32",
+        label: "Create Student",
+      },
+    ],
+  },
+];
+
+const MainLayout = () => {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            height: "4rem",
+            flexWrap: "wrap",
+          }}
+        >
+          <h2>PH University</h2>
+        </div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]} items={items} />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <h1>The content should be here</h1>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Ant Design ©{new Date().getFullYear()} Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
