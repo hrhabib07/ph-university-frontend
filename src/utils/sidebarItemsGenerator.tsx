3d17974789d10f0183e9b86a93e60713b923ccce
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-13 16:30:48 +0600

    sidebar issue resolved

--- src/utils/sidebarItemsGenerator.tsx
+++ src/utils/sidebarItemsGenerator.tsx
@@ -1,17 +1,34 @@
-import { TSidebarItems, TUserPath } from "../types";
+import { TSidebarItem, TUserPath } from "../types";
 import { NavLink } from "react-router-dom";
 
-export const sidebarItemsGenerator = (data: TUserPath[], role: string): TSidebarItems[] => {
-  return data.map((item) => {
-    const newItem: TSidebarItems = {
-      key: item.name,
-      label: item.children ? item.name : <NavLink to={`/${role}/${item.path}`}>{item.name}</NavLink>,
-    };
-
-    if (item.children) {
-      newItem.children = sidebarItemsGenerator(item.children, role);
+export const sidebarItemsGenerator = (items: TUserPath[], role: string) => {
+  const sidebarItems = items.reduce((acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/${role}/${item.path}`}>{item.name}</NavLink>,
+      });
     }
 
-    return newItem;
-  });
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => {
+          if (child.name) {
+            return {
+              key: child.name,
+              label: (
+                <NavLink to={`/${role}/${child.path}`}>{child.name}</NavLink>
+              ),
+            };
+          }
+        }),
+      });
+    }
+
+    return acc;
+  }, []);
+
+  return sidebarItems;
 };
