7f7b01d5618ff1b1247d50201e190b611b78fe29
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-20 00:17:03 +0600

    registerd semesters data table update status button implemented

--- src/pages/Admin/CourseManagement/RegisteredSemesters.tsx
+++ src/pages/Admin/CourseManagement/RegisteredSemesters.tsx
@@ -1,9 +1,157 @@
-const RegisteredSemesters = () => {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  Button,
+  Dropdown,
+  Space,
+  Table,
+  TableColumnsType,
+  TableProps,
+  Tag,
+} from "antd";
+import { TAcademicSemester } from "../../../types";
+import { SetStateAction, useState } from "react";
+import { DownOutlined } from "@ant-design/icons";
+
+import { useGetAllRegisteredSemesterQuery } from "../../../redux/features/admin/courseManagement/courseManagement";
+import moment from "moment";
+
+type TTableData = Pick<
+  TAcademicSemester,
+  "_id" | "name" | "year" | "startMonth" | "endMonth"
+>;
+
+const items = [
+  { label: "UPCOMING", value: "UPCOMING" },
+  { label: "ONGOING", value: "ONGOING" },
+  { label: "ENDED", value: "ENDED" },
+];
+
+const RegisteredSemester = () => {
+  const [params, setParams] = useState();
+  const { data, isFetching } = useGetAllRegisteredSemesterQuery(params);
+  console.log(data);
+  const handleStatusDropdown = (data) => {
+    console.log(data);
+  };
+  const menuProps = {
+    items,
+    onClick: handleStatusDropdown,
+  };
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      showSorterTooltip: { target: "full-header" },
+      filters: [
+        {
+          text: "Fall",
+          value: "Fall",
+        },
+        {
+          text: "Autumn",
+          value: "Autumn",
+        },
+        {
+          text: "Summer",
+          value: "Summer",
+        },
+      ],
+
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      onFilter: (value, record) => record.name.indexOf(value as string) === 0,
+    },
+
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (item) => {
+        let color;
+        // console.log("item : ", item);
+        if (item === "ONGOING") {
+          color = "green";
+        }
+        if (item === "UPCOMING") {
+          color = "blue";
+        }
+        if (item === "ENDED") {
+          color = "red";
+        }
+        return <Tag color={color}>{item}</Tag>;
+      },
+    },
+    {
+      title: "Start Date",
+      dataIndex: "startDate",
+    },
+    {
+      title: "End Date",
+      dataIndex: "endDate",
+    },
+    {
+      title: "End Date",
+      dataIndex: "endDate",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: (item) => {
+        // console.log("item", item);
+        return (
+          <Space>
+            <Dropdown menu={menuProps}>
+              <Button>
+                <Space>
+                  {item.status}
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+          </Space>
+        );
+      },
+      width: "1%",
+    },
+  ];
+
+  const onChange: TableProps<TTableData>["onChange"] = (
+    pagination: any,
+    filters: any,
+    sorter: any,
+    extra: any
+  ) => {
+    // console.log(extra, filters);
+    const queryParams:
+      | SetStateAction<undefined>
+      | { name: string; value: any }[] = [];
+    if (extra.action === "filter") {
+      filters.name?.forEach((item: any) =>
+        queryParams.push({ name: "name", value: item })
+      );
+      filters.year?.forEach((item: any) =>
+        queryParams.push({ name: "year", value: item })
+      );
+    }
+    setParams(queryParams);
+  };
+
+  const tableData = data?.data?.map((item) => ({
+    key: item._id,
+    name: `${item.academicSemester.name} ${item.academicSemester.year}`,
+    status: item.status,
+    startDate: moment(new Date(item.startDate)).format("MMMM Do YYYY"), // Converts startDate to a readable format
+    endDate: moment(new Date(item.startDate)).format("MMMM Do YYYY"),
+  }));
   return (
-    <div>
-      <h1>Hello, From RegisteredSemesters!</h1>
-    </div>
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
   );
 };
 
-export default RegisteredSemesters;
+export default RegisteredSemester;
--- src/redux/features/admin/courseManagement/courseManagement.ts
+++ src/redux/features/admin/courseManagement/courseManagement.ts
@@ -2,20 +2,20 @@ import { baseApi } from "../../../api/baseApi";
 
 const courseManagementApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    // getAllAcademicSemester: builder.query({
-    //   query: (args) => {
-    //     const params = new URLSearchParams();
-    //     console.log(args);
-    //     if (args) {
-    //       args.forEach((item) => params.append(item.name, item.value));
-    //     }
-    //     return {
-    //       url: "/academic-semesters",
-    //       method: "GET",
-    //       params,
-    //     };
-    //   },
-    // }),
+    getAllRegisteredSemester: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        console.log(args);
+        if (args) {
+          args.forEach((item) => params.append(item.name, item.value));
+        }
+        return {
+          url: "/semester-registrations",
+          method: "GET",
+          params,
+        };
+      },
+    }),
     addRegisteredSemester: builder.mutation({
       query: (data) => ({
         url: "/semester-registrations/create-semester-registration",
@@ -26,4 +26,7 @@ const courseManagementApi = baseApi.injectEndpoints({
   }),
 });
 
-export const { useAddRegisteredSemesterMutation } = courseManagementApi;
+export const {
+  useAddRegisteredSemesterMutation,
+  useGetAllRegisteredSemesterQuery,
+} = courseManagementApi;
